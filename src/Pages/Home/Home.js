853cf2aacd228dc2ad0f2a585947767fffc76467
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-30 00:00:26 +0600

    tailwind css added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2>Home</h2>
+            <h1 className="text-3xl font-bold text-center underline">
+                Hello world!
+            </h1>
         </div>
     );
 };
