8dc9b16aec19020e7715443da7caa1dda3fbd50b
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-30 14:32:56 +0600

    login page

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const Banner = () => {
-    const navigate = useNavigate();
-
     return (
         <div>
             <header>
