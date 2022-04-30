33a8896877645ceef173be36f9f5d480bf71f22c
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-30 21:02:51 +0600

    social media login

--- src/Components/SocialMediaLogin/SocialMediaLogin.js
+++ src/Components/SocialMediaLogin/SocialMediaLogin.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithFacebook, useSignInWithGithub, useSignInWithGoogle, useSignInWithTwitter } from 'react-firebase-hooks/auth';
 import { toast } from 'react-toastify';
 import auth from '../../Hooks/Firebase.Init';
 import { BsGoogle, BsLinkedin, BsFacebook, BsGithub, BsTwitter, BsInstagram } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
 
 const SocialMediaLogin = () => {
+    const navigate = useNavigate();
     const [
         signInWithGoogle,
         googleUser,
@@ -12,28 +14,41 @@ const SocialMediaLogin = () => {
         googleError
     ] = useSignInWithGoogle(auth);
 
-    if (googleError) {
+    const [
+        signInWithGithub,
+        githubUser,
+        githubLoading,
+        githubError
+    ] = useSignInWithGithub(auth);
+
+    const [
+        signInWithFacebook,
+        facebookUser,
+        facebookLoading,
+        facebookError
+    ] = useSignInWithFacebook(auth);
+
+    const [
+        signInWithTwitter,
+        twitterUser,
+        twitterLoading,
+        twitterError
+    ] = useSignInWithTwitter(auth);
+
+    if (googleError || githubError || facebookError || twitterError) {
         toast(googleError?.message);
     }
-    if (googleLoading) {
+    if (googleLoading || githubLoading || facebookLoading || twitterLoading) {
         return <p>Loading...</p>;
     }
-    if (googleUser) {
+    if (googleUser || githubUser || facebookUser || twitterUser) {
         toast('Welcome Back');
+        navigate('/');
     }
 
     return (
         <>
-            <button
-                type="button"
-                data-mdb-ripple="true"
-                data-mdb-ripple-color="light"
-                class="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
-            >
-                <BsLinkedin className='text-xl' />
-            </button>
-
-            <button
+            <button onClick={() => signInWithGithub()}
                 type="button"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
@@ -51,7 +66,7 @@ const SocialMediaLogin = () => {
                 <BsGoogle className='text-xl' />
             </button>
 
-            <button onClick={signInWithGoogle}
+            <button onClick={() => signInWithFacebook()}
                 type="button"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
@@ -60,16 +75,7 @@ const SocialMediaLogin = () => {
                 <BsFacebook className='text-xl' />
             </button>
 
-            <button onClick={signInWithGoogle}
-                type="button"
-                data-mdb-ripple="true"
-                data-mdb-ripple-color="light"
-                class="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
-            >
-                <BsInstagram className='text-xl' />
-            </button>
-
-            <button onClick={signInWithGoogle}
+            <button onClick={() => signInWithTwitter()}
                 type="button"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
