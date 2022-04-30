371870efac707a887dfb003d115a14afda65d0ca
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-30 18:21:06 +0600

    terms checkbox

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const [agree, setAgree] = useState(false);
+
+    const register = event => {
+        event.preventDefault();
+
+        const firstName = event.target.firstName.value;
+        const lastName = event.target.lastName.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        const confirmPassword = event.target.confirmPassword.value;
+
+        if (password === confirmPassword && agree) {
+            console.log(firstName, lastName, email, password, confirmPassword, agree);
+        }
+    }
     return (
-        <section class="h-screen">
+        <section class="h-screen m-24">
             <div class="px-6 h-full text-gray-800">
                 <div
                     class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
@@ -18,7 +33,7 @@ const Register = () => {
                         />
                     </div>
                     <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
-                        <form>
+                        <form onSubmit={register}>
                             <div class="flex flex-row items-center justify-center lg:justify-start">
                                 <p class="text-lg mb-0 mr-4">Register with</p>
                                 <button
@@ -75,7 +90,7 @@ const Register = () => {
                                     <input
                                         type="text"
                                         class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                        id="formFirstName"
+                                        id="formFirstName" name='firstName'
                                         placeholder="First Name"
                                     />
                                 </div>
@@ -84,7 +99,7 @@ const Register = () => {
                                     <input
                                         type="text"
                                         class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                        id="formLastName"
+                                        id="formLastName" name='lastName'
                                         placeholder="Last Name"
                                     />
                                 </div>
@@ -94,7 +109,7 @@ const Register = () => {
                                 <input
                                     type="text"
                                     class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                    id="formEmail"
+                                    id="formEmail" name='email'
                                     placeholder="Email address"
                                 />
                             </div>
@@ -103,7 +118,7 @@ const Register = () => {
                                 <input
                                     type="password"
                                     class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                    id="formPassword"
+                                    id="formPassword" name='password'
                                     placeholder="Password"
                                 />
                             </div>
@@ -112,19 +127,19 @@ const Register = () => {
                                 <input
                                     type="password"
                                     class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                    id="formConfirmPassword"
+                                    id="formConfirmPassword" name='confirmPassword'
                                     placeholder="Confirm Password"
                                 />
                             </div>
 
                             <div class="flex justify-between items-center mb-6">
                                 <div class="form-group form-check">
-                                    <input
+                                    <input onClick={() => setAgree(!agree)}
                                         type="checkbox"
-                                        class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                                        id="exampleCheck2"
+                                        class="form-check-input appearance-none h-4 w-4 border border-red-600 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                        id="exampleCheck2" name='terms'
                                     />
-                                    <label class="form-check-label inline-block text-gray-800" for="exampleCheck2"
+                                    <label class={`form-check-label inline-block ${agree ? 'text-gray-800' : 'text-red-600'}`} for="exampleCheck2"
                                     >Agree to our terms & condition</label
                                     >
                                 </div>
@@ -133,8 +148,8 @@ const Register = () => {
 
                             <div class="text-center lg:text-left">
                                 <button
-                                    type="button"
-                                    class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                                    type="submit"
+                                    class="button inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                                 >
                                     Register
                                 </button>
