fa484183d24efe23482cc3d99db5d72ca97a14f3
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-30 19:48:33 +0600

    login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import auth from '../../Hooks/Firebase.Init';
 
 const Login = () => {
+    const navigate = useNavigate();
+    const [formError, setFormError] = useState('');
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loginLoading,
+        loginError,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const login = event => {
+        event.preventDefault();
+
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        signInWithEmailAndPassword(email, password);
+
+        toast('Welcome Back');
+    }
+
+    if (loginError) {
+        setFormError(loginError?.message);
+    }
+    if (loginLoading) {
+        return <p>Loading...</p>;
+    }
+    if (user) {
+        navigate('/');
+    }
+
     return (
         <section class="h-screen">
             <div class="px-6 h-full text-gray-800">
@@ -18,7 +51,7 @@ const Login = () => {
                         />
                     </div>
                     <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
-                        <form>
+                        <form onSubmit={login}>
                             <div class="flex flex-row items-center justify-center lg:justify-start">
                                 <p class="text-lg mb-0 mr-4">Sign in with</p>
                                 <button
@@ -74,7 +107,7 @@ const Login = () => {
                                 <input
                                     type="text"
                                     class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                    id="exampleFormControlInput2"
+                                    id="formEmail" name='email'
                                     placeholder="Email address"
                                 />
                             </div>
@@ -83,7 +116,7 @@ const Login = () => {
                                 <input
                                     type="password"
                                     class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                    id="exampleFormControlInput2"
+                                    id="formPassword" name='password'
                                     placeholder="Password"
                                 />
                             </div>
@@ -103,9 +136,10 @@ const Login = () => {
                             </div>
 
                             <div class="text-center lg:text-left">
+                                <p className='mb-4 text-red-600'>{formError}</p>
                                 <button
-                                    type="button"
-                                    class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                                    type="submit"
+                                    class="button inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                                 >
                                     Login
                                 </button>
