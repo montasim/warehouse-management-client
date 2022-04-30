d7d47b6ef39364a61db2d39fedd2e7ef8e9e442e
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-30 17:01:50 +0600

    404 page

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import e404 from '../../Media/Image/e404.png';
 
 const NotFound = () => {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <h2>NotFound</h2>
+        <div className='p-24'>
+            <h2 className='text-center text-3xl uppercase text-grey-600'>The page you are looking is not found!</h2>
+            <button onClick={() => navigate('/')} type="button" class="block px-6 py-2.5 bg-gray-800 text-white font-medium text-xs rounded-full mx-auto mt-8 mb-16">Back to Home</button>
+            <img src={e404} class="max-w-full h-auto mx-auto" alt="e404 image" />
         </div>
     );
 };
