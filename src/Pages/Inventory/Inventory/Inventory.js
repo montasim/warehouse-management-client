48d7f6eafe4b8cb88b39c7c79e1a5fff841a4bd8
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-01 22:15:01 +0600

    display item details

--- src/Pages/Inventory/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory/Inventory.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { MdOutlineBrandingWatermark, MdOutlineLocalShipping } from 'react-icons/md';
+import { FcRating } from 'react-icons/fc';
+import { AiOutlineDollar, AiOutlineStock } from 'react-icons/ai';
+import { ImPriceTag } from 'react-icons/im';
 
 const Inventory = () => {
     const _id = useParams();
     const [product, setProduct] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch(`https://posdash-server.herokuapp.com/inventory/${_id?.id}`)
@@ -11,12 +16,74 @@ const Inventory = () => {
             .then(data => setProduct(data));
     }, []);
 
-    console.log(_id.id);
+    const { category, name, description, supplierName, price, stock, ratings, ratingsCount, img, shipping, quantity } = product;
 
     return (
-        <div>
-            <h2>Inventory {product.name}</h2>
-        </div>
+        <div className='d-block mx-auto p-12'>
+            <h2 className="text-3xl font-bold uppercase">Details Of {name}</h2>
+            <div className="container my-24 px-6 mx-auto">
+                <section className="mb-32 text-gray-800 text-center md:text-left">
+                    <div className="block rounded-lg shadow-lg bg-white">
+                        <div className="flex flex-wrap items-center">
+                            <div className="grow-0 shrink-0 basis-auto block lg:flex w-full lg:w-6/12 xl:w-4/12">
+                                <img src={img} alt="Trendy Pants and Shoes"
+                                    className="w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg" />
+                            </div>
+                            <div className="grow-0 shrink-0 basis-auto w-full lg:w-6/12 xl:w-8/12">
+                                <div className="px-6 py-12 md:px-12">
+                                    <h2 className="text-3xl font-bold pb-2">{name}</h2>
+                                    <h4 className='text-xl pb-2'> Supplier: {supplierName}</h4>
+                                    <p className="text-gray-500 mb-6 pb-2">
+                                        {description}
+                                    </p>
+                                    <div className="flex flex-wrap mb-6">
+                                        <div className="w-full lg:w-6/12 xl:w-4/12 mb-4">
+                                            <p className="flex items-center justify-center md:justify-start">
+                                                <MdOutlineBrandingWatermark className='mr-3' /> Category: {category}
+                                            </p>
+                                        </div>
+                                        <div className="w-full lg:w-6/12 xl:w-4/12 mb-4">
+                                            <p className="flex items-center justify-center md:justify-start">
+                                                <ImPriceTag className='mr-3' />
+                                                Unit Price: {price}
+                                            </p>
+                                        </div>
+                                        <div className="w-full lg:w-6/12 xl:w-4/12 mb-4">
+                                            <p className="flex items-center justify-center md:justify-start">
+                                                <AiOutlineStock className='mr-3' />
+                                                In Stock: {stock}
+                                            </p>
+                                        </div>
+                                        <div className="w-full lg:w-6/12 xl:w-4/12 mb-4">
+                                            <p className="flex items-center justify-center md:justify-start">
+                                                <FcRating className='mr-3' />
+                                                Ratings: 5 / {ratings}
+                                            </p>
+                                        </div>
+                                        <div className="w-full lg:w-6/12 xl:w-4/12 mb-4">
+                                            <p className="flex items-center justify-center md:justify-start">
+                                                <AiOutlineDollar className='mr-3' />
+                                                Total Price: {stock * price}
+                                            </p>
+                                        </div>
+                                        <div className="w-full lg:w-6/12 xl:w-4/12 mb-4">
+                                            <p className="flex items-center justify-center md:justify-start">
+                                                <MdOutlineLocalShipping className='mr-3' />
+                                                Shipped: {shipping}
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <button type="button"
+                                        className="inline-block px-7 py-3 bg-gray-800 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out">
+                                        Delivered
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div >
+        </div >
     );
 };
 
