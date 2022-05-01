341ec5cf7ea1540bd21e6916fda47a97a1a42899
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-01 10:32:05 +0600

    class is replaced with className

--- src/Components/SocialMediaLogin/SocialMediaLogin.js
+++ src/Components/SocialMediaLogin/SocialMediaLogin.js
@@ -52,7 +52,7 @@ const SocialMediaLogin = () => {
                 type="button"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
-                class="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
+                className="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
             >
                 <BsGithub className='text-xl' />
             </button>
@@ -61,7 +61,7 @@ const SocialMediaLogin = () => {
                 type="button"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
-                class="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
+                className="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
             >
                 <BsGoogle className='text-xl' />
             </button>
@@ -70,7 +70,7 @@ const SocialMediaLogin = () => {
                 type="button"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
-                class="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
+                className="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
             >
                 <BsFacebook className='text-xl' />
             </button>
@@ -79,7 +79,7 @@ const SocialMediaLogin = () => {
                 type="button"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
-                class="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
+                className="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
             >
                 <BsTwitter className='text-xl' />
             </button>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -37,78 +37,78 @@ const Login = () => {
     }
 
     return (
-        <section class="h-screen">
-            <div class="px-6 h-full text-gray-800">
+        <section className="h-screen">
+            <div className="px-6 h-full text-gray-800">
                 <div
-                    class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
+                    className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
                 >
                     <div
-                        class="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
+                        className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
                     >
                         <img
                             src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                            class="w-full"
+                            className="w-full"
                             alt="Sample image"
                         />
                     </div>
-                    <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+                    <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
                         <form onSubmit={login}>
-                            <div class="flex flex-row items-center justify-center lg:justify-start">
-                                <p class="text-lg mb-0 mr-4">Sign in with</p>
+                            <div className="flex flex-row items-center justify-center lg:justify-start">
+                                <p className="text-lg mb-0 mr-4">Sign in with</p>
                                 <SocialMediaLogin />
                             </div>
 
                             <div
-                                class="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
+                                className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
                             >
-                                <p class="text-center font-semibold mx-4 mb-0">Or</p>
+                                <p className="text-center font-semibold mx-4 mb-0">Or</p>
                             </div>
 
-                            <div class="mb-6">
+                            <div className="mb-6">
                                 <input
                                     type="text"
-                                    class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                     id="formEmail" name='email'
                                     placeholder="Email address"
                                 />
                             </div>
 
-                            <div class="mb-6">
+                            <div className="mb-6">
                                 <input
                                     type="password"
-                                    class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                     id="formPassword" name='password'
                                     placeholder="Password"
                                 />
                             </div>
 
-                            <div class="flex justify-between items-center mb-6">
-                                <div class="form-group form-check">
+                            <div className="flex justify-between items-center mb-6">
+                                <div className="form-group form-check">
                                     <input
                                         type="checkbox"
-                                        class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                        className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                                         id="exampleCheck2"
                                     />
-                                    <label class="form-check-label inline-block text-gray-800" for="exampleCheck2"
+                                    <label className="form-check-label inline-block text-gray-800" htmlFor="exampleCheck2"
                                     >Remember me</label
                                     >
                                 </div>
-                                <Link to="/forgot-password" class="text-blue-700">Forgot password?</Link>
+                                <Link to="/forgot-password" className="text-blue-700">Forgot password?</Link>
                             </div>
 
-                            <div class="text-center lg:text-left">
+                            <div className="text-center lg:text-left">
                                 <p className='mb-4 text-red-600'>{formError}</p>
                                 <button
                                     type="submit"
-                                    class="button inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                                    className="button inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                                 >
                                     Login
                                 </button>
-                                <p class="text-sm font-semibold mt-2 pt-1 mb-0">
+                                <p className="text-sm font-semibold mt-2 pt-1 mb-0">
                                     Don't have an account?
                                     <Link
                                         to="/register"
-                                        class="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
+                                        className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
                                     > Register</Link
                                     >
                                 </p>
