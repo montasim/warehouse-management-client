653d1eeca4c0cfdef192d0841a1b9e2178038179
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-02 17:10:04 +0600

    testimonial icon

--- src/Pages/Home/Testimonials/Testimonials/Testimonials.js
+++ src/Pages/Home/Testimonials/Testimonials/Testimonials.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Testimonial from '../Testimonial/Testimonial';
+import { MdOutlineReviews } from 'react-icons/md';
 
 const Testimonials = () => {
     const [testimonials, setTestimonials] = useState([]);
@@ -13,7 +14,10 @@ const Testimonials = () => {
     return (
         <section className="my-28 text-gray-700 px-4">
             <div className="text-center max-w-3xl mx-auto">
-                <h3 className="text-3xl font-bold mb-6 text-gray-800">Testimonials</h3>
+                <h3 className="flex items-center justify-center text-3xl font-bold mb-6 text-gray-800">
+                    <MdOutlineReviews className='text-3xl mr-3' />
+                    Testimonials
+                </h3>
                 <p className="mb-6 pb-2 md:mb-12">
                     POSData is a world class inventory management system. We provide live support to all of our user. Hear more from our honorable users.
                 </p>
